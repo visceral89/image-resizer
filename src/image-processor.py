3a4a23dd3c1286c98760140efe1fcb01a9c6b611
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-07-31 10:01:29 +0200

    Rewriting Imageprocessor

--- src/image-processor.py
+++ src/image-processor.py
@@ -1,18 +1,28 @@
 from PIL import Image, ImageSequence
+import os
 
 
 def resize_image(image_path, output_size):
     with Image.open(image_path) as img:
-        resized_img = img.resize(output_size)
-        return resize_image
+        return img.resize(output_size, Image.LANCZOS)
 
 
 def resize_gif(image_path, output_size):
     with Image.open(image_path) as img:
-        frames = []
+        frames = [
+            frame.convert("RGBA").resize(output_size, Image.LANCZOS)
+            for frame in ImageSequence.Iterator(img)
+        ]
+    return frames
 
-        for frame in ImageSequence.Iterator(img):
-            frame = frame.convert("RGBA")
-            resized_frame = frame.resize(output_size)
-            frames.append(resized_frame)
-    return resized_frame
+
+def save_images(image, output_path):
+    if isinstance(image, list):
+        image[0].save(output_path, save_all=True, append_images=image[1:], loop=1)
+    else:
+        image.save(output_path)
+
+
+def process_images(folder_path, output_sizes):
+    output_folder = os.path.join(folder_path, "resized")
+    os.makedirs(output_folder, exist_ok=True)
